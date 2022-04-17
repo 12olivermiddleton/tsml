b06933b8bd70ca24619559d6d30b26b5f852eb7a
Oliver Middleton <=>
2022-04-17 14:27:30 +0100

    - Ig attribute split measures - include boolean to decide which measure to use

--- src/main/java/ml_6002b_coursework/IGAttributeSplitMeasure.java
+++ src/main/java/ml_6002b_coursework/IGAttributeSplitMeasure.java
@@ -1,5 +1,6 @@
 package ml_6002b_coursework;
 
+import experiments.data.DatasetLoading;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -12,33 +13,29 @@ import java.io.IOException;
 
 public class IGAttributeSplitMeasure extends AttributeSplitMeasure {
 
-    /**
-     * Loads the arff file at the target location and sets the last attribute to be the class value,
-     * or returns null on any error, such as not finding the file or it being malformed
-     *
-     * @param fullPath path to the file to try and load
-     * @return Instances from file.
-     */
-    public static Instances loadData(String fullPath) throws IOException {
-        return loadDataThrowable(new File(fullPath));
-    }
 
     @Override
     public double computeAttributeQuality(Instances data, Attribute att) throws Exception {
 
-        int[][] peaty = new int[data.attribute("Peaty").numValues()][data.numClasses()];
+        int attribute_index = att.index();
 
+        int[][] att_cont_table = new int[data.attribute(attribute_index).numValues()][data.numClasses()];
 
-        System.out.println(peaty);
         for(Instance ins:data){
-            peaty[(int)ins.value(0)][(int)ins.classValue()]++;
+            att_cont_table[(int)ins.value(attribute_index)][(int)ins.classValue()]++;
         }
-        for(int[] x:peaty) {
+        for(int[] x:att_cont_table) {
             for (int y : x)
                 System.out.print(y + ",");
             System.out.print("\n");
         }
+        System.out.println(att_cont_table[0][0] + ":" + att_cont_table[0][1] + ":" + att_cont_table[1][0] + ":" + att_cont_table[1][1]);
 
+        AttributeMeasures am = new AttributeMeasures();
+        double ig = am.measureInformationGain(att_cont_table);
+        double igr = am.measureInformationGainRatio(att_cont_table);
+        System.out.println("ig: "+ig);
+        System.out.println("igr: "+ igr);
 
 
         return 0;
@@ -51,17 +48,14 @@ public class IGAttributeSplitMeasure extends AttributeSplitMeasure {
      */
     public static void main(String[] args) throws Exception {
         String WhiskeyData = "C:/Users/omidd/OneDrive/Documents/University/Third Year/Machine Learning/tsml/src/main/java/ml_6002b_coursework/Whiskey_Region_Data.arff";
-        Instances whiskey = LoadData(WhiskeyData);
-//        int num = whiskey.numClasses();
-//        System.out.println(num);
-        System.out.println(whiskey.numClasses());
+        Instances whiskey = DatasetLoading.loadData(WhiskeyData);
 
-//        IGAttributeSplitMeasure ig = new IGAttributeSplitMeasure();
-//        Attribute Peaty = whiskey.attribute("Peaty");
-//
-////        Instances[] whiskey_split = ig.splitData(whiskey, Peaty);
-//        double attribute_quality = ig.computeAttributeQuality(whiskey, Peaty);
-//        System.out.println("Attribute Quality: " + attribute_quality);
+        IGAttributeSplitMeasure ig = new IGAttributeSplitMeasure();
+        Attribute Peaty = whiskey.attribute("Peaty");
+
+//        Instances[] whiskey_split = ig.splitData(whiskey, Peaty);
+        double attribute_quality = ig.computeAttributeQuality(whiskey, Peaty);
+        System.out.println("Attribute Quality: " + attribute_quality);
 
     }
 
