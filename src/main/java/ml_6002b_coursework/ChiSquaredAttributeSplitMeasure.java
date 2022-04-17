00e87e2904894db36bc1bf6de0c98765d80f3d90
Oliver Middleton <=>
2022-04-17 11:02:49 +0100

    - Added whiskey data as an arff

--- src/main/java/ml_6002b_coursework/ChiSquaredAttributeSplitMeasure.java
+++ src/main/java/ml_6002b_coursework/ChiSquaredAttributeSplitMeasure.java
@@ -3,8 +3,24 @@ package ml_6002b_coursework;
 import weka.core.Attribute;
 import weka.core.Instances;
 
+import java.io.FileReader;
+
 
 public class ChiSquaredAttributeSplitMeasure extends AttributeSplitMeasure{
+
+    public static Instances LoadData(String file_path){
+
+        Instances dataset = null;
+        try{
+            FileReader reader = new FileReader(file_path);
+            dataset = new Instances(reader);
+        }catch(Exception e) {
+            System.out.println("Exception caught: " + e);
+        }
+
+        return dataset;
+    }
+
 
     @Override
     public double computeAttributeQuality(Instances data, Attribute att) throws Exception {
--- src/main/java/ml_6002b_coursework/GiniAttributeSplitMeasure.java
+++ src/main/java/ml_6002b_coursework/GiniAttributeSplitMeasure.java
@@ -3,8 +3,23 @@ package ml_6002b_coursework;
 import weka.core.Attribute;
 import weka.core.Instances;
 
+import java.io.FileReader;
+
 
 public class GiniAttributeSplitMeasure extends AttributeSplitMeasure{
+
+    public static Instances LoadData(String file_path){
+
+        Instances dataset = null;
+        try{
+            FileReader reader = new FileReader(file_path);
+            dataset = new Instances(reader);
+        }catch(Exception e) {
+            System.out.println("Exception caught: " + e);
+        }
+
+        return dataset;
+    }
 
     @Override
     public double computeAttributeQuality(Instances data, Attribute att) throws Exception {
@@ -21,6 +36,17 @@ public class GiniAttributeSplitMeasure extends AttributeSplitMeasure{
      * @param args the options for the split measure main
      */
     public static void main(String[] args) {
-        System.out.println("Not Implemented.");
+
+        String WhiskeyData = "C:/Users/omidd/OneDrive/Documents/University/Third Year/Machine Learning/tsml/src/main/java/ml_6002b_coursework/Whiskey_Region_Data.arff";
+        Instances whiskey = LoadData(WhiskeyData);
+        System.out.println(whiskey);
+        GiniAttributeSplitMeasure gini = new GiniAttributeSplitMeasure();
+        Instances[] whiskey_split = gini.splitData(whiskey, Peaty);
+        System.out.println(whiskey_split);
+
+
+
     }
+
+
 }
